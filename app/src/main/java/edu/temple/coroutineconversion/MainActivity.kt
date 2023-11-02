6f778b32b92e843ce61f4a6e346756ff59d55008
Brandon <tuk16746@temple.edu>
2023-11-02 14:35:06 -0400

    Changed to couroutines

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +24,9 @@ class MainActivity : AppCompatActivity() {
         true
     })
 
+    val scope = CoroutineScope(Job() + Dispatchers.Default)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,12 +34,19 @@ class MainActivity : AppCompatActivity() {
         cakeImageView = findViewById(R.id.imageView)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+            scope.launch {
+                changeAlpha()
+            }
+
+        }
+    }
+
+    suspend fun changeAlpha() {
+        repeat(100){
+            withContext(Dispatchers.Main){
+                cakeImageView.alpha = it/100f
+            }
+            delay(40)
         }
     }
 }
